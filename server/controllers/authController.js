c0e76d0b8e541ab05cc7cff7f7be5a82976dfcd1
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-18 17:31:25 +0530

    password encrypted

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,12 @@
-const loginUser = (req, res) => {
+const User = require("../db/models/User");
+
+const loginUser = async (req, res) => {
 	res.json({ msg: "Login Successful" });
 };
 
-const registerUser = (req, res) => {
-	res.json({ msg: "Register Successful" });
+const registerUser = async (req, res) => {
+	const user = await User.create(req.body);
+	res.status(200).json(user);
 };
 
 module.exports = { loginUser, registerUser };
