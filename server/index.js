5fe4109877ba839ceb84bad7bd792d4e774da54e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-19 17:54:31 +0530

    not found middleware added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 require("./db/connectDB");
+require("express-async-errors");
+
 const express = require("express");
+const notFoundMiddleware = require("./middleware/notFoundMiddleware");
 const authRouter = require("./routes/authRoute");
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRouter);
+
+//errors middleware
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 6000;
 
