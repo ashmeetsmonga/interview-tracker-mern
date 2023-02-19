f7ba7e8b969d32f962cf9c59f1d77bbfc1c3dee9
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-19 18:12:26 +0530

    added jwt token in login and register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,23 +1,28 @@
+const { StatusCodes } = require("http-status-codes");
 const User = require("../db/models/User");
 const AuthenticationError = require("../errors/AuthenticationError");
-const NotFoundError = require("../errors/NotFoundError");
 
 const loginUser = async (req, res) => {
 	const { email, password } = req.body;
 
 	const user = await User.findOne({ email });
-
 	if (!user) throw new AuthenticationError("Email not found");
 
 	const isPasswordCorrect = await user.checkPassword(password);
 	if (!isPasswordCorrect) throw new AuthenticationError("Credentials are incorrect");
-	res.status(200).json(user);
+
+	const token = user.createJWT();
+
+	res.status(StatusCodes.OK).json({ name: user.name, token });
 };
 
 const registerUser = async (req, res) => {
 	const { name, email, password } = req.body;
 	const user = await User.create(req.body);
-	res.status(200).json(user);
+
+	const token = user.createJWT();
+
+	res.status(StatusCodes.OK).json({ name, token });
 };
 
 module.exports = { loginUser, registerUser };
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = mongoose.Schema({
 	name: {
@@ -27,6 +28,13 @@ UserSchema.pre("save", async function (next) {
 UserSchema.methods.checkPassword = async function (inputPassword) {
 	const isPasswordCorrect = await bcrypt.compare(inputPassword, this.password);
 	return isPasswordCorrect;
+};
+
+UserSchema.methods.createJWT = function () {
+	const token = jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	});
+	return token;
 };
 
 module.exports = mongoose.model("User", UserSchema);
