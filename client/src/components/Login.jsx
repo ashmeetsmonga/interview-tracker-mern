72404f43ae97da53ad40fc3a2303cf2b10c6c76a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-20 22:39:01 +0530

    ui changes in login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 	const loginUser = async (e) => {
 		e.preventDefault();
 		try {
-			const data = await axios.post("http://localhost:5000/auth/login", { email, password });
+			const { data } = await axios.post("http://localhost:5000/auth/login", { email, password });
 			console.log(data);
 		} catch (err) {
 			console.log(err);
@@ -18,23 +18,23 @@ const Login = () => {
 	return (
 		<div className='w-1/3 bg-gray-800 rounded-lg flex flex-col gap-8 items-center p-8'>
 			<p className='text-5xl text-white font-bold uppercase tracking-widest'>Login</p>
-			<form className='flex flex-col items-center gap-4' onSubmit={loginUser}>
+			<form className='w-4/5 flex flex-col items-center gap-4' onSubmit={loginUser}>
 				<input
 					type='text'
 					placeholder='email'
-					className='p-2 text-xl tracking-widest rounded-md outline-none focus:outline-none'
+					className='w-full text-center p-2 text-xl tracking-widest rounded-md outline-none focus:outline-none'
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<input
 					type='text'
 					placeholder='password'
-					className='p-2 text-xl tracking-widest rounded-md outline-none focus:outline-none'
+					className='w-full text-center p-2 text-xl tracking-widest rounded-md outline-none focus:outline-none'
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<button
-					className='bg-white text-gray-800 p-2 mt-4 rounded-md font-bold text-2xl tracking-wider uppercase'
+					className='text-white p-2 mt-2 rounded-md font-bold text-2xl tracking-wider uppercase'
 					type='submit'
 				>
 					Submit
