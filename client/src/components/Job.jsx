44634093fc4ce1b6d2597869f309c50910769aec
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-21 17:02:36 +0530

    color to green if interview cleared

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Job = ({ data }) => {
 	return (
 		<div className='w-1/4 bg-gray-50 flex flex-col gap-2 rounded-sm shadow-2xl'>
-			<div className='bg-gray-800 text-gray-100 font-semibold rounded-t-sm text-2xl tracking-widest p-4'>
+			<div
+				className={`${
+					data.status.toLowerCase() === "cleared" ? "bg-green-800" : "bg-gray-800"
+				} text-gray-100 font-semibold rounded-t-sm text-2xl tracking-widest p-4`}
+			>
 				{data.company}
 			</div>
 			<div className='flex flex-col gap-2 p-4'>
